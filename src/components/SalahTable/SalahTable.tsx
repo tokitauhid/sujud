7d9bb7c2d44fec3160de96ef4fbe28b2d06ffc86
Toki Tauhid <tokitauhidmim12@gmail.com>
2026-09-10 20:03:19 +0600

    Revert "fix(layout): make prayer tracker table fully responsive in tablet portrait mode"
    This reverts commit 33d228fdb2baa04bf6e7d7437c98a1794fd6d2e0.

--- src/components/SalahTable/SalahTable.tsx
+++ src/components/SalahTable/SalahTable.tsx
@@ -260,7 +260,7 @@ const SalahTable = ({
           </motion.div>
         )}
       </AnimatePresence>
-      <div className="h-[95%] w-full min-w-0">
+      <div className="h-[95%]">
         <AutoSizer>
           {({ height, width }) => (
             <Table
